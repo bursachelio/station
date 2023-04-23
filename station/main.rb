d09d97f7e809fc61905e25c58163310b8be298b1
Ivan <115031282+bursachelio@users.noreply.github.com>
2023-04-23 15:41:18 +0300

    Add files via upload

---
+++ station/main.rb
@@ -0,0 +1,159 @@
+require_relative 'train'
+require_relative 'route'
+require_relative 'station'
+#require_relative 'wagons'
+
+trains = []
+routes = []
+stations = []
+
+loop do
+  puts "Выберите действие:"
+  puts "1 - Создать станцию"
+  puts "2 - Создать поезд"
+  puts "3 - Создать маршрут"
+  puts "4 - Управление маршрутом"
+  puts "5 - Назначить маршрут поезду"
+  puts "6 - Добавить вагон к поезду"
+  puts "7 - Отцепить вагон от поезда"
+  puts "8 - Переместить поезд по маршруту"
+  puts "9 - Просмотреть список станций"
+  puts "10 - Просмотреть список поездов на станции"
+  puts "0 - Выход"
+  choice = gets.chomp.to_i
+
+  case choice
+  when 1
+    puts "Введите название станции:"
+    name = gets.chomp
+    station = Station.new(name)
+    puts "Станция #{station.name} создана"
+    stations << station
+  when 2
+    puts "Введите номер поезда:"
+    num = gets.chomp.to_i
+    puts "Введите тип поезда:"
+    type = gets.chomp
+    train = Train.new(num, type, speed = 0)
+    puts "Поезд с номером #{train.num} типа #{type} создан"
+    trains << train
+  when 3
+    puts "Введите начальную станцию маршрута:"
+    start_station = gets.chomp
+    puts "Введите конечную станцию маршрута:"
+    finish_station = gets.chomp
+    route = Route.new(start_station, finish_station)
+    puts "Маршрут #{route.start_station} - #{route.finish_station} создан"
+    routes << route
+  when 4
+    puts "Выберите маршрут:"
+    routes.each_with_index { |route, index| puts "#{index + 1} - #{route.start_station} - #{route.finish_station}" }
+    choice = gets.chomp.to_i
+    route = routes[choice - 1]
+    loop do
+      puts "Выберите действие:"
+      puts "1 - Добавить станцию"
+      puts "2 - Удалить станцию"
+      puts "0 - Выход"
+      choice = gets.chomp.to_i
+      case choice
+      when 1
+        puts "Введите название станции:"
+        name = gets.chomp
+        station = Station.new(name)
+        route.add_station(station)
+        puts "Станция #{station.name} добавлена в маршрут"
+      when 2
+        puts "Введите название станции:"
+        name = gets.chomp
+        route.del_station(name)
+        puts "Станция #{name} удалена из маршрута"
+      when 0
+        break
+      else
+        puts "Неверный выбор"
+      end
+    end
+  when 5
+    puts "Выберите поезд:"
+    trains.each_with_index { |train, index| puts "#{index + 1} - #{train.num}" }
+    train_choice = gets.chomp.to_i
+    train = trains[train_choice - 1]
+
+    puts "Выберите маршрут:"
+    routes.each_with_index { |route, index| puts "#{index + 1} - #{route.start_station} - #{route.finish_station}" }
+    route_choice = gets.chomp.to_i
+    route = routes[route_choice - 1]
+
+    train.assign_route(route)
+    puts "Маршрут назначен поезду #{train.num}"
+
+  when 6
+    puts "Выберите поезд:"
+    trains.each_with_index { |train, index| puts "#{index + 1} - #{train.num}" }
+    train_choice = gets.chomp.to_i
+    train = trains[train_choice - 1]
+
+    if train.type == 'pass'
+      puts "Введите количество мест в вагоне:"
+      seats = gets.chomp.to_i
+      wagon = PassengerWagon.new(seats)
+    elsif train.type == 'cargo'
+      puts "Введите объем вагона:"
+      volume = gets.chomp.to_f
+      wagon = CargoWagon.new(volume)
+    end
+
+    train.add_vagon(wagon)
+    puts "Вагон добавлен к поезду #{train.num}"
+
+  when 7
+    puts "Выберите поезд:"
+    trains.each_with_index { |train, index| puts "#{index + 1} - #{train.num}" }
+    train_choice = gets.chomp.to_i
+    train = trains[train_choice - 1]
+
+    train.detach_wagon
+    puts "Вагон отцеплен от поезда #{train.num}"
+
+  when 8
+    puts "Выберите поезд:"
+    trains.each_with_index { |train, index| puts "#{index + 1} - #{train.num}" }
+    train_choice = gets.chomp.to_i
+    train = trains[train_choice - 1]
+
+    loop do
+      puts "Выберите направление:"
+      puts "1 - Вперед"
+      puts "2 - Назад"
+      puts "0 - Выход"
+      direction = gets.chomp.to_i
+
+      case direction
+      when 1
+        train.go_next_station
+        puts "Поезд #{train.num} #{train.route_info}"
+      when 2  
+        train.go_back_station
+        puts "Поезд #{train.num} #{train.route_info}"
+      when 0
+        break
+      else
+        puts "Неверный выбор"
+      end
+    end
+
+  when 9
+    puts "Список станций:"
+    stations.each_with_index { |station, index| puts "#{index + 1} - #{station}" }
+
+  when 10
+    puts "Выберите станцию:"
+    stations.each_with_index { |station, index| puts "#{index + 1} - #{station}" }
+    choice = gets.chomp.to_i
+    station = stations[choice - 1]
+    puts "Поезда на станции #{station}:"
+    station.trains.each { |train| puts "#{train.num} - #{train.type}, количество вагонов: #{train.carriages.count}" }
+  end
+  
+end
---
+++ station/train.rb
@@ -0,0 +1,67 @@
+class Train
+  attr_reader :route, :current_station_index, :vagons
+  attr_accessor :num, :speed
+
+  def initialize(num, type, speed = 0)
+    @num = num
+    @type = type
+    @speed = speed
+    @vagons = []
+  end
+
+  def speed_plus(speed)
+    @speed = speed
+  end
+
+  def stop(speed = 0)
+    @speed = speed
+  end
+
+  def assign_route(route)
+    @route = route
+    @current_station_index = 0
+    puts "Маршрут поезда назначен, поезд находится на станции #{route.stations[current_station_index]}"
+  end
+
+  def next_station
+    route.stations[current_station_index + 1]
+  end
+
+  def back_station
+    route.stations[current_station_index - 1]
+  end
+
+  def go_next_station
+    @current_station_index += 1 if next_station
+  end
+
+  def go_back_station
+    @current_station_index -= 1 if back_station
+  end
+
+  def route_info
+    if current_station_index > 0 && current_station_index != route.stations.length - 1
+      puts "Поезд находится на станции #{route.stations[current_station_index]}, предыдущая станция #{route.stations[current_station_index - 1]}, следущая станция #{route.stations[current_station_index + 1]} "
+    elsif current_station_index > 0 && current_station_index == route.stations.length - 1
+      puts "Предыдущая станция #{route.stations[current_station_index - 1]}, поезд находится на последней станции #{route.stations[current_station_index]}, просьба выйти из вагона. Уважаемые пассажиры, обращаем ваше внимание, что за нахождение в поезде, следующем в тупик, предусмотрена административная ответственность в соответствии с законодательством Российской Федерации."
+    elsif current_station_index == 0
+      puts "Поезд находится на первой станции #{route.stations[current_station_index]}, следущая станция #{route.stations[current_station_index + 1]}"
+    end
+  end
+
+  def add_vagon(vagon)
+    if @speed == 0 && train.type == vagon.type
+      @vagons << vagon
+    else
+      puts "Невозможно добавить вагон"
+    end
+  end
+  
+  def remove_vagon
+    if @speed == 0 && !vagons.empty?
+      @vagons.pop
+    else
+      puts "Невозможно отцепить вагон"
+    end
+  end
+end
